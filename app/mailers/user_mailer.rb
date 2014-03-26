644ebd303b77f26338d7a66eb0d54f57e96dfc24
Paolo Cosentino <cosepaol88@gmail.com>
2014-03-25 20:42:08 -0500

    contact page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,7 @@ class UserMailer < ActionMailer::Base
   def send_email(user_info)
     @name = user_info[:name]
     @email = user_info[:email]
+    @phone = user_info[:phone]
     @description = user_info[:description]
     mail(:to => "pcosentino88@gmail.com", :subject => "Portfolio Notification")
   end
