c121897daacfbc10d8a4e9292e1023934c6d1868
Paolo Cosentino <cosepaol88@gmail.com>
2014-04-29 09:52:02 -0500

    new imgs

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,8 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-     :address              => "smtp.gmail.com",
+     :address              => "smtp.mail.yahoo.com",
      :port                 => 25,
-     :domain               => "smtp.mail.yahoo.com",
      :user_name            => 'freedomfp2006@sbcglobal.net',
      :password             => 'morgan120973',
-     :authentication       => 'plain',
-:enable_starttls_auto => true }
+     :authentication       => :plain,
+		 :enable_starttls_auto => true }
