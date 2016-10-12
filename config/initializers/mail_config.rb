735ba16d3d4c0539261ec26444a87423ba569d8a
Paolo Cosentino <cosepaol88@gmail.com>
2016-10-11 21:03:43 -0500

    revert back

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,12 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-:address => "smtp.sendgrid.net",
-:port    => 25,
-:authentication => :plain,
-:user_name => 'freedomfp2006@sbcglobal.net',
-:password => 'morgan120973',
-:domain => "heroku.com"
-:enable_starttls_auto => true 
-}
-ActionMailer::Base.delivery_method = :smtp
-
-
+     :address              => "smtp.mail.yahoo.com",
+     :port                 => 25,
+     :user_name            => 'freedomfp2006@sbcglobal.net',
+     :password             => 'morgan120973',
+     :authentication       => :plain,
+		 :enable_starttls_auto => true }
