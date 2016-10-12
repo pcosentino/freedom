301d620f4bc6a65bbeb8c8f421544190f919b6d6
Paolo Cosentino <cosepaol88@gmail.com>
2016-10-11 20:40:23 -0500

    add sendgrid

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ end
 group :production do
 	gem 'pg', '0.15.1'
 	gem 'rails_12factor', '0.0.2'
+	gem 'sendgrid-ruby'
 end
 
 # Use SCSS for stylesheets
--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,7 +1,18 @@
 ActionMailer::Base.smtp_settings = {
-     :address              => "smtp.mail.yahoo.com",
-     :port                 => 25,
-     :user_name            => 'freedomfp2006@sbcglobal.net',
-     :password             => 'morgan120973',
-     :authentication       => :plain,
-		 :enable_starttls_auto => true }
+:address => "smtp.sendgrid.net",
+:port    => 25,
+:authentication => :plain,
+:user_name => 'freedomfp2006@sbcglobal.net',
+:password => 'morgan120973',
+:domain => "heroku.com"
+:enable_starttls_auto => true 
+}
+ActionMailer::Base.delivery_method = :smtp
+
+
+:port => 587,
+
+:user_name => ENV["SENDGRID_USERNAME"],
+:password => ENV["SENDGRID_PASSWORD"],
+
+
