db7d8e59b1fe4981606730f081f58ce3df0ff69f
Paolo Cosentino <cosepaol88@gmail.com>
2016-10-11 21:00:50 -0500

    added back to normal

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -2,10 +2,11 @@ ActionMailer::Base.smtp_settings = {
 :address => "smtp.sendgrid.net",
 :port => 587,
 :authentication => :plain,
-:user_name => 'apikey',
-:password => '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',
+:user_name => 'freedomfp2006@sbcglobal.net',
+:password => 'morgan120973',
 :domain => "heroku.com"
 :enable_starttls_auto => true 
 }
 ActionMailer::Base.delivery_method = :smtp
+
 
